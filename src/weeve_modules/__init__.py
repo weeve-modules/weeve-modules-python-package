45e6547e721e54fddd9b752bcc978af96fd4370a
kubagrzelak <jakubgrzelak1@gmail.com>
2023-06-01 22:17:27 +0100

    Moved importing request_handler() to __init__.

--- src/weeve_modules/__init__.py
+++ src/weeve_modules/__init__.py
@@ -6,6 +6,9 @@ from .logger import initialize_logging, weeve_logger
 # initialize logging
 initialize_logging()
 
+# initialize HTTP POST endpoint for incoming data
+from weeve_modules.request_handler import request_handler
+
 from .send import send
 from .listener import listener
 from .add_graceful_termination import add_graceful_termination
--- src/weeve_modules/listener.py
+++ src/weeve_modules/listener.py
@@ -6,7 +6,6 @@ import sys
 from os import getenv
 from bottle import run
 from weeve_modules.logger import weeve_logger
-from weeve_modules.request_handler import request_handler
 from weeve_modules.listener_callback_function import set_listener_callback_function
 
 # set up logging
@@ -17,8 +16,8 @@ def listener(callback_function: object = None) -> None:
     Set up a listener to weeve intercontainer communication and save a user defined callback function where received data by this module is passed to for further processing.
 
     Args:
-        callback_function (object): The callback function defined by a user and ready to receive data for further processing. Callback function must accept data as an argument. 
-                                    It should accept data in JSON format, as that is the format used by other modules within weeve ecosystem. 
+        callback_function (object): The callback function defined by a user and ready to receive data for further processing. Callback function must accept data as an argument.
+                                    It should accept data in JSON format, as that is the format used by other modules within weeve ecosystem.
                                     The function should be also responsible for handling errors that could happen during processing following best software engineering practices.
     """
 
